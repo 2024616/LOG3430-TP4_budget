8473d92555422c1cf47dee894df87738688472a8
2024616 <www1202161@gmail.com>
2024-11-10 16:24:37 -0500

    add bisect in pipeline

--- myscript.py
+++ myscript.py
@@ -1,4 +1,7 @@
 import os
 
+badhash = os.popen("git log -1 --format=%H").read().strip()
+goodhash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
+
 os.system("git bisect start $badhash $goodhash")
-os.system("git bisect run ...")
+os.system("git bisect run test")
